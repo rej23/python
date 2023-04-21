48be9b2f25ee44751867bfe4fbb603e8606099ab
rej23 <rej.amada@gmail.com>
2023-04-21 09:47:33 +0100

    2

---
+++ lab4.py
@@ -0,0 +1,7 @@
+books = {"abraham": ["first book", "second book"], "mary": ["first book", "second book"], "author3": ["book1","book2"]}
+
+
+print ("author name: Abraham, mary, author3")
+a = input ("name of author:")
+
+print(",".join(books[a]))
